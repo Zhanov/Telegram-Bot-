4dd460b4220e43e444f95c310a5f9b9aa23c9bdd
main <zheksinbi@mail.ru>
2025-03-01 21:44:18 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import telebot
+import requests
+import json 
+
+bot = telebot.TeleBot('7446225545:AAEulIZcxS6kc41QqYHHmk7l9dnLKJ_63tM')
+API = '5b4874614f4c7bfc878d18ec472ddf3f'
+
+# откликание на стартовое сообщение 
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, "Салем ")
+
+@bot.message_handler(content_types=['text'])
+def get_wheather(messege):
+    city = messege.text.strip().lower()
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    data = json.loads(response.text)
+    bot.reply_to(messege, f'Сейчас погода в {city}: {data["main"]["temp"]}')
+
+bot.polling(non_stop=True)
